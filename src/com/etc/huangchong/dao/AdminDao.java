82af578ae6deddb5c1b5051f732ae0148062cf87
Administrator <Administrator@stu1003>
2018-10-11 16:44:48 +0800

    测试更新

--- src/com/etc/huangchong/dao/AdminDao.java
+++ src/com/etc/huangchong/dao/AdminDao.java
@@ -3,7 +3,11 @@ package com.etc.huangchong.dao;
 import java.util.List;
 
 import com.etc.huangchong.entity.Admin;
-
+/**
+ * 管理员dao
+ * @author Administrator
+ *
+ */
 public interface AdminDao {
 	List<Admin> queyrAdmin();
 
