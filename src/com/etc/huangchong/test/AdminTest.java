f16d63be1713e23d47b4f991a281f8a662271dde
Administrator <Administrator@stu1003>
2018-10-11 19:30:35 +0800

    管理员测试类

---
+++ src/com/etc/huangchong/test/AdminTest.java
@@ -0,0 +1,19 @@
+package com.etc.huangchong.test;
+
+import java.util.List;
+
+import com.etc.huangchong.entity.Admin;
+import com.etc.huangchong.service.AdminService;
+import com.etc.huangchong.service.impl.AdminServiceImpl;
+
+public class AdminTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AdminService as = new AdminServiceImpl();
+		List<Admin> list = as.getQueryAdmin();
+		list.forEach(System.out::println);
+	
+	}
+
+}
