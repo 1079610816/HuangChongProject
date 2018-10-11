453af934fc31eab77d87a667fdf17571bbe23e1d
Administrator <Administrator@stu1003>
2018-10-11 18:23:17 +0800

    增加用户的Servlet，删除user相关的html，更新user实体类和测试类

--- src/com/etc/huangchong/test/UsersTest.java
+++ src/com/etc/huangchong/test/UsersTest.java
@@ -5,7 +5,11 @@ import java.util.List;
 import com.etc.huangchong.entity.Users;
 import com.etc.huangchong.service.UsersService;
 import com.etc.huangchong.service.impl.UsersServiceImpl;
-
+/**
+ * 用户的测试类
+ * @author Administrator
+ *
+ */
 public class UsersTest {
 
 	public static void main(String[] args) {
