e8249c1aa18ee13ed022bc3fcd4084e3534554c4
Administrator <Administrator@stu1003>
2018-10-11 19:52:09 +0800

    更新了BaseDao

--- src/com/etc/huangchong/util/BaseDao.java
+++ src/com/etc/huangchong/util/BaseDao.java
@@ -22,7 +22,7 @@ public class BaseDao {
 	private static final String URL = "jdbc:mysql://192.168.9.230:3306/homestay?useunicode=true&characterEncoding=utf-8";
 	private static final String USER = "root"; // 用户名
 	private static final String PASSWORD = "ROOT";// 密码
-
+ 
 	/**
 	 * 获取连接对象
 	 * 
