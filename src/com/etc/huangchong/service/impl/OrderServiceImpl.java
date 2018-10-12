7d513d0c34f25f22563285fdb97937755f13a024
Administrator <Administrator@stu1003>
2018-10-12 20:45:54 +0800

    实现了OrderSevice中的删除方法getDelOrder()

--- src/com/etc/huangchong/service/impl/OrderServiceImpl.java
+++ src/com/etc/huangchong/service/impl/OrderServiceImpl.java
@@ -10,10 +10,22 @@ import com.etc.huangchong.service.OrderService;
 public class OrderServiceImpl implements OrderService {
 
 	private OrderDao od = new OrderDaoImpl();
+
+	/**
+	 * 查询订单列表
+	 */
 	@Override
 	public List<Orders> getQueryOrder() {
 		// TODO Auto-generated method stub
 		return od.queryOrder();
 	}
 
+	/**
+	 * 删除单条订单记录
+	 */
+	@Override
+	public boolean getDelOrder(int orderId) {
+		// TODO Auto-generated method stub
+		return od.delOrder(orderId);
+	}
 }
