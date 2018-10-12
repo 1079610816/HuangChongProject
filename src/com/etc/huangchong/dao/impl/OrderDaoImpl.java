930c88685cfea8e1f9ae6a5ec07d4cde7f8e2851
Administrator <Administrator@stu1003>
2018-10-12 15:53:32 +0800

    修改了OrderDaoImpl中的sql

--- src/com/etc/huangchong/dao/impl/OrderDaoImpl.java
+++ src/com/etc/huangchong/dao/impl/OrderDaoImpl.java
@@ -11,7 +11,7 @@ public class OrderDaoImpl implements OrderDao {
 @Override
 public List<Orders> queryOrder() {
 	// TODO Auto-generated method stub
-	String sql = "SELECT * FROM orderdetails";
+	String sql = "SELECT orders.orderId, orderdetails.bookTime, orderdetails.unsubscribeTime, orderdetails.fee, orderdetails.orderStatus, users.userName, accommodation.accomTitle FROM orders ,orderdetails ,users ,accommodation WHERE orders.orderId = orderdetails.orderId AND orders.accomId = accommodation.accomId AND orders.userId = users.userId";
 	List<Orders> list = (List<Orders>)BaseDao.select(sql, Orders.class);
 	return list;
 }
