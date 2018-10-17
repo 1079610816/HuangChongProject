bfacf724d8193cc45979b00eb77aed3e4d894ebb
Administrator <Administrator@stu1003>
2018-10-17 13:37:09 +0800

    用户的servlet

--- src/com/etc/huangchong/controller/FrontStageUsersServlet.java
+++ src/com/etc/huangchong/controller/FrontStageUsersServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import com.etc.huangchong.entity.Users;
 import com.etc.huangchong.service.UsersService;
 import com.etc.huangchong.service.impl.UsersServiceImpl;
+import com.etc.huangchong.util.IndustrySMS;
 import com.etc.huangchong.util.MD5Util;
 
 /**
@@ -41,38 +42,100 @@ public class FrontStageUsersServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
 		String op = request.getParameter("op");
-		PrintWriter out=response.getWriter();
+		PrintWriter out = response.getWriter();
 
 		if ("loginByPwd".equals(op)) {
-			
-			String username = request.getParameter("username");
+
+			String userName = request.getParameter("userName");
 
 			String userPwd = request.getParameter("userPwd");
-			System.out.println(username+userPwd);
-			
-			userPwd=MD5Util.getEncodeByMd5(userPwd);
-			
+
+			userPwd = MD5Util.getEncodeByMd5(userPwd);
+
 			String validationCode = request.getParameter("validationCode");
 
 			HttpSession session = request.getSession();
 
 			String validation_code = (String) session.getAttribute("validation_code");
-    
+
 			if (validationCode.equalsIgnoreCase(validation_code)) {
-				System.out.println(username+userPwd);
-				Users users = us.getLogin(username, userPwd);
-				System.out.println(users);
-				if (null != users) {
-					session.setAttribute("users", users);
+
+				Users user = us.getLogin(userName, userPwd);
+
+				if (null != user) {
+					session.setAttribute("user", user);
 					out.print("2");
-//					request.getRequestDispatcher("index.html").forward(request, response);
 				} else {
 					out.print("1");
-//					request.getRequestDispatcher("login.jsp").forward(request, response);
 				}
 			} else {
 				out.print("0");
 
+			}
+			out.close();
+		} else if ("getByTelNum".equals(op)) {
+			String validationCode1 = request.getParameter("validationCode1");
+			HttpSession session = request.getSession();
+			// 获得原始的图形验证码
+			String validation_code = (String) session.getAttribute("validation_code");
+			
+			if(validationCode1.equalsIgnoreCase(validation_code)) {
+				
+				int number_code = (int) ((Math.random() * 9 + 1) * 100000);
+				
+				String to = request.getParameter("telNum");
+				
+				String result = IndustrySMS.execute(to, number_code);
+				if (null != result) {
+					session.setAttribute("number", number_code);
+					out.print("1");
+				} else {
+					out.print("0");
+				}
+			}else {
+				out.print("2");
+			}			
+			out.close();
+		} else if ("loginByMsg".equals(op)) {
+			// 获得电话号码
+			String telNum = request.getParameter("telNum");
+			// 获得图形验证码
+			String validationCode1 = request.getParameter("validationCode1");
+			// 获得手机验证码
+			String loginCode = request.getParameter("loginCode");
+
+			System.out.println("telNum:" + telNum + ",loginCode" + loginCode + ",validationCode1:" + validationCode1);
+			// 创建HttpSession对象
+			HttpSession session = request.getSession();
+			// 获得原始的图形验证码
+			String validation_code = (String) session.getAttribute("validation_code");
+			
+			System.out.println(validation_code);
+			// 获得原始的手机验证码
+			Integer number_code = (Integer) session.getAttribute("number");
+			// 转换手机验证码的数据类型
+			String number = number_code.toString();
+
+			System.out.println(number);
+
+			if (validationCode1.equalsIgnoreCase(validation_code) && loginCode.equals(number)) {
+				// 图形验证码和手机验证码都正确 执行登录
+				Users user = us.getLoginByMsg(telNum);
+				System.out.println(user);
+				if (null != user) {
+					// 用户不为空 ，将user放进session中
+					session.setAttribute("user", user);
+					out.print("3");
+				} else {
+					// 为空，执行自动增加用户
+					out.print("2");
+				}
+			} else if (validationCode1.equalsIgnoreCase(validation_code)) {
+				// 手机验证码错误
+				out.print("1");
+			} else {
+				// 图形验证码错误
+				out.print("0");
 			}
 			out.close();
 		}
