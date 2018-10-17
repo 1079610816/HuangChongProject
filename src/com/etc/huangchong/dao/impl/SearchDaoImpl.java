7a1917b4b519628013fffd41c90cc19fcbd4316c
Administrator <Administrator@stu1003>
2018-10-17 19:25:15 +0800

    搜索页更新

--- src/com/etc/huangchong/dao/impl/SearchDaoImpl.java
+++ src/com/etc/huangchong/dao/impl/SearchDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.etc.huangchong.dao.SearchDao;
 import com.etc.huangchong.entity.Homestay;
 import com.etc.huangchong.util.BaseDao;
+import com.etc.huangchong.util.PageData;
 
 public class SearchDaoImpl implements SearchDao {
 
@@ -16,4 +17,10 @@ public class SearchDaoImpl implements SearchDao {
 		return (List<Homestay>)bd.select(sql, Homestay.class, accomArea);
 	}
 
+	@Override
+	public PageData querySearchPage(int page, int pageSize,String accomArea) {
+		// TODO Auto-generated method stub
+		String sql = "select a.accomId,a.accomTitle,a.accomIntro,a.accomArea,a.accomAddress,a.accomStatus,a.houseType,a.price,u.userName,COUNT(c.accomId) as commentsum from accommodation as a inner join users as u on a.userId=u.userId left join comment as c on a.accomId=c.accomId where a.accomArea=? group by a.accomId";
+		return bd.getPage(sql, page, pageSize, Homestay.class, accomArea);
+	}
 }
--- src/com/etc/huangchong/test/SearchTest.java
+++ src/com/etc/huangchong/test/SearchTest.java
@@ -5,13 +5,17 @@ import java.util.List;
 import com.etc.huangchong.dao.SearchDao;
 import com.etc.huangchong.dao.impl.SearchDaoImpl;
 import com.etc.huangchong.entity.Homestay;
+import com.etc.huangchong.util.PageData;
 
 public class SearchTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SearchDao sd = new SearchDaoImpl();
-		List<Homestay> list = sd.querySearch("厦门");
+		//List<Homestay> list = sd.querySearch("厦门");
+		//list.forEach(System.out::println);
+		PageData pd = sd.querySearchPage(2, 5, "厦门");
+		List<Homestay> list = pd.getData();
 		list.forEach(System.out::println);
 
 	}
