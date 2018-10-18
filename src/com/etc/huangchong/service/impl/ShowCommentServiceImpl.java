e4dd1c9ce016d00cb3e407530b40c598f39e1059
Administrator <Administrator@stu1003>
2018-10-18 19:24:14 +0800

    ssd

---
+++ src/com/etc/huangchong/service/impl/ShowCommentServiceImpl.java
@@ -0,0 +1,18 @@
+package com.etc.huangchong.service.impl;
+
+import java.util.List;
+
+import com.etc.huangchong.dao.ShowCommentDao;
+import com.etc.huangchong.dao.impl.ShowCommentDaoImpl;
+import com.etc.huangchong.entity.ShowComment;
+import com.etc.huangchong.service.ShowCommentService;
+
+public class ShowCommentServiceImpl implements ShowCommentService {
+	ShowCommentDao scd=new ShowCommentDaoImpl();
+	@Override
+	public List<ShowComment> getQueryShowComment(int userId) {
+		List<ShowComment> list = scd.queryComment(userId);
+		return list;
+	}
+
+}
